8264bd5f465a93119fcafe22c48463862c1e47f4
GuravPratik <pratikgurav1010@gmail.com>
2023-09-25 08:59:52 +0530

    add lib and basic setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,38 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./components/PageNotFound";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Homepage />} />
+
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "#fff",
+            color: "#374151",
+          },
+        }}
+      />
+    </>
+  );
 }
